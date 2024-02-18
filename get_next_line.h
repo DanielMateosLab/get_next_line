b8398a1d26bd7fe4ad7e4cdb44f519e9e810db66
damateos <damateos@student.42malaga.com>
2024-02-18 19:28:44 +0100

    rename resize_line signature args

--- get_next_line.h
+++ get_next_line.h
@@ -23,7 +23,7 @@
 char	*get_next_line(int fd);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 int		get_newline_i(const char *s);
-void	resize_line(char **buff, size_t new_size);
+void	resize_line(char **line, size_t new_size);
 size_t	ft_strlen(char const *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	read_until_nl_eof(char **line, int fd);
